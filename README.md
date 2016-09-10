31bf903b7095f1cde8d823b89d14527875e8243c
Rob Sanderson <azaroth42@gmail.com>
2016-09-09 18:24:49 -0700

    Initial alpha 0.0001 version of the C/ORM

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Python-CIDOC-ORM
 Python library to make creation of CIDOC CRM easier by mapping classes/predicates to python objects
+
+# Status
+Very Alpha.  Extracted the interesting bits from IIIF-Prezi and refactored to be a bit stricter.
+Parses an extraction of the CRM ontology as TSV to generate class hierarchy and property/predicate mapping.
+
+Collaboration and feedback welcome!
---
+++ cidoc_orm.py
@@ -0,0 +1,439 @@
+
+from __future__ import unicode_literals
+import os, sys, subprocess
+import codecs
+import inspect
+
+try:
+    import json
+except:
+    # Fallback for 2.5
+    import simplejson as json
+
+try:
+    # Only available in 2.7+
+    # This makes the code a bit messy, but eliminates the need
+    # for the locally hacked ordered json encoder
+    from collections import OrderedDict
+except:
+    # Backported...
+    try:
+        from ordereddict import OrderedDict
+    except:
+        raise Exception("To run with old pythons you must: easy_install ordereddict")
+
+try:
+	subprocess.check_output #should be OK in python2.7 up
+except:
+	#python2.6, see <http://python-future.org/standard_library_imports.html>
+	from future.standard_library import install_aliases
+	install_aliases()
+
+try:
+	STR_TYPES = [str, unicode] #Py2
+except:
+	STR_TYPES = [bytes, str] #Py3
+
+
+class CidocError(Exception):
+	"""Base exception for iiif_prezi."""
+
+	resource = None
+
+	def __init__(self, msg, resource=None):
+		"""Initialize CidocError."""
+		self.args = [msg]
+		self.resource = resource
+
+
+class ConfigurationError(CidocError):
+	"""Raised when an object (likely the factory) isn't configured properly for the current operation."""
+	pass
+
+
+class MetadataError(CidocError):
+	"""Base metadata exception."""
+	pass
+
+
+class StructuralError(MetadataError):
+	"""Raised when there are structural problem with an object/metadata."""
+	pass
+
+
+class RequirementError(MetadataError):
+	"""Raised when metadata requirements not met."""
+	pass
+
+
+class DataError(MetadataError):
+	"""Raised when metadata is not valid/allowed."""
+	pass
+
+class CidocFactory(object):
+
+	def __init__(self, baseUrl="", baseDir="", lang="en"):
+		self.baseUrl = baseUrl
+		self.baseDir = baseDir
+		self.lang = lang
+
+		self.context_uri = ""
+
+		self.debug_level = "warn"
+		self.log_stream = sys.stderr
+
+		self.namespaces = {}
+		self.class_map = {}
+		self.property_map = {}
+
+
+		self.key_order_hash = {}
+
+
+	def set_debug_stream(self, strm):
+		"""Set debug level."""
+		self.log_stream = strm
+
+	def set_debug(self, typ):
+		"""Set behavior on errors and warnings.
+
+		error = squash warnings
+		warn = display warnings
+		error_on_warning = raise exception for a warning rather than continuing
+		"""
+		if typ in ['error', 'warn', 'error_on_warning']:
+			self.debug_level = typ
+		else:
+			raise ConfigurationError("Only levels are 'error', 'warn' and 'error_on_warning'")
+
+	def maybe_warn(self, msg):
+		"""warn method that respects debug_level property."""
+		if self.debug_level == "warn":
+			self.log_stream.write(msg + "\n")
+			try:	
+				self.log_stream.flush()
+			except:
+				pass
+		elif self.debug_level == "error_on_warning":
+			# We don't know the type, just raise a MetadataError
+			raise MetadataError(msg)
+
+factory = CidocFactory("http://data.getty.edu/provenance/")
+
+class BaseResource(object):
+	"""Base class for all resources."""
+
+	_properties = {}
+	_integer_properties = []
+	_object_properties = []
+	_text_properties = ["label", "has_note"]
+	_required_properties = []
+	_warn_properties = []
+	_uri_segment = ""
+
+	def __init__(self, ident="", label="", **kw):
+		"""Initialize BaseObject."""
+		self._factory = factory
+		if ident:
+			if ident.startswith('http'):
+				self.id = ident
+			else:
+				self.id = factory.baseUrl + self.__class__._uri_segment + "/" + ident
+
+		else:
+			self.id = ""
+		self.type = self.__class__._type
+		self.label = label
+
+	def __setattr__(self, which, value):
+		"""Attribute setting magic for error checking and resource/literal handling."""
+		try:
+			types = [str, unicode, list, dict] #Py2
+		except: 
+			types = [bytes, str, list, dict] #Py3
+
+		if which == 'context':
+			raise DataError("Must not set directly")
+		elif which[0] != "_" and not self._check_prop(which, value):
+			raise DataError("Setting non-standard field '%s' on resource of type '%s'" % (which, self._type))
+
+		if hasattr(self, which) and hasattr(self, 'set_%s' % which):
+			fn = getattr(self, 'set_%s' % which)
+			return fn(value)
+		elif value and which in self._object_properties:
+			self._set_magic_resource(which, value)
+		else:			
+			object.__setattr__(self, which, value)
+
+	def _check_prop(self, which, value):
+		for c in self._classhier:
+			if c._properties.has_key(which):
+				rng = c._properties[which]['range']
+				if rng == str:					
+					return True
+				elif isinstance(value, rng):
+					return True
+				else:
+					return False
+		return False
+			
+	def maybe_warn(self, msg):
+		"""warn that respects debug settings."""
+		msg = "WARNING: " + msg
+		self._factory.maybe_warn(msg)
+
+	def test_object(self, data):
+		"""True if data is an object (resource or URI)."""
+		# "http://..."
+		# {"@id": "http://..."}
+		# or list of above
+		if type(data) in STR_TYPES:
+			return data.startswith('http')
+		elif type(data) == dict:
+			return 'id' in data
+		elif isinstance(data, BaseMetadataObject):
+			return True
+		elif type(data) == list:
+			for d in data:
+				if type(d) in STR_TYPES and not data.startswith('http'):
+					return False
+				elif type(d) == dict and not 'id' in d:
+					return False
+			return True
+		else:
+			print("expecing a resource, got: %r" % (data))
+			return True
+	
+	def _set_magic_lang(self, which, value):
+		"""Magical handling of languages for string properties."""
+		# Should this actually do magic still?
+		object.__setattr__(self, which, value)
+
+	def _set_magic_resource(self, which, value):
+		"""Set resource property.
+		allow: string/object/dict, and magically generate list thereof
+		"""
+		try:
+			current = getattr(self, which)
+		except:
+			current = None
+		if not current:
+			object.__setattr__(self, which, value)
+		elif type(current) == list:
+			new = current.append(value)
+			object.__setattr__(self, which, new)
+		else:
+			new = [current, value]
+			object.__setattr__(self, which, new)
+
+
+	def toJSON(self, top=False):
+		"""Serialize as JSON."""
+		d = self.__dict__.copy()
+
+		for (k, v) in list(d.items()): #list makes copy in Py3
+			if not v or k[0] == "_":
+				del d[k]
+		if 'context' in d:
+			d['@context'] = d['context']
+			del d['context']
+		for e in self._required_properties:
+			if e not in d:
+				raise RequirementError("Resource type '%s' requires '%s' to be set" % (self._type, e), self)
+		debug = self._factory.debug_level
+		if debug.find("warn") > -1:
+			for e in self._warn_properties:
+				if e not in d:
+					msg = "Resource type '%s' should have '%s' set" % (self._type, e)
+					self.maybe_warn(msg)
+		if top:
+			d['@context'] = self._factory.context_uri
+
+		# Recurse into structures, maybe minimally
+		for (p,sinfo) in self._properties.items():
+			if p in d:
+				if type(d[p]) == list:
+					newl = []
+					for s in d[p]:
+						minimalOveride = self._should_be_minimal(s)
+						done = self._single_toJSON(s, sinfo, p, minimalOveride)
+						newl.append(done)
+					d[p] = newl
+				else:
+					if sinfo.get('list', False):
+						raise StructuralError("%s['%s'] must be a list, got %r" % (self._type, p, d[p]), self)
+					d[p] = self._single_toJSON(d[p], sinfo, p)
+
+		KOH = self._factory.key_order_hash
+		return OrderedDict(sorted(d.items(), key=lambda x: KOH.get(x[0], 1000)))
+
+	def _should_be_minimal(self, what):
+		"""Return False."""
+		return False
+
+	def _single_toJSON(self, instance, sinfo, prop, minimalOveride=False):
+		# duck typing. Bite me. 
+		typ = sinfo.get('subclass', None)
+		minimal = sinfo.get('minimal', False)
+		if minimalOveride:
+			minimal=True
+		if type(instance) in STR_TYPES:
+			# Just a URI
+			return instance
+		elif ( isinstance(instance, BaseResource) and typ == None ) or (typ != None and isinstance(instance, typ)):
+			if minimal:
+				return {'id': instance.id, 'type': instance._type, 'label': instance.label}
+			else:
+				return instance.toJSON(False)
+		elif type(instance) == dict and ( ('type' in instance and instance['type'] == typ._type) or typ == None ):
+			if minimal:
+				return {'id': instance['id'], 'type':instance['type'], 'label': instance['label']}
+			else:
+				return instance
+		elif type(instance) == dict:
+			raise StructuralError("%s['%s'] objects must be of type %s, got %s" % (self._type, prop, typ._type, instance.get('@type', None)), self)
+		else:
+			raise StructuralError("Saw unknown object in %s['%s']: %r" % (self._type, prop, instance), self)
+
+	def _buildString(self, js, compact=True):
+		"""Build string from JSON."""
+		if type(js) == dict:
+			if compact:
+				out = json.dumps(js, sort_keys=True, separators=(',',':'))
+			else:
+				out = json.dumps(js, sort_keys=True, indent=2)
+		else:
+			if compact:
+				out = json.dumps(js, separators=(',',':'))
+			else:
+				out = json.dumps(js, indent=2)
+		return out 		
+
+	def toString(self, compact=True):
+		"""Return JSON setialization as string."""
+		js = self.toJSON(top=True)
+		return self._buildString(js, compact)
+
+	def toFile(self, compact=True):
+		"""Write to local file.
+
+		Creates directories as necessary
+		"""
+		mdd = self._factory.baseDir
+		if not mdd:
+			raise ConfigurationError("Directory on Factory must be set to write to file")
+
+		js = self.toJSON(top=True)
+		# Now calculate file path based on URI of top object
+		# ... which is self for those of you following at home
+		myid = js['id']
+		mdb = self._factory.metadata_base
+		if not myid.startswith(mdb):
+			raise ConfigurationError("The id of that object is not the base URI in the Factory")
+		fp = myid[len(mdb):]	
+		bits = fp.split('/')
+		if len(bits) > 1:
+			mydir = os.path.join(mdd, '/'.join(bits[:-1]))		
+			try:
+				os.makedirs(mydir)
+			except OSError:
+				pass
+		fh = open(os.path.join(mdd, fp), 'w')
+		out = self._buildString(js, compact)
+		fh.write(out)
+		fh.close()
+		return out
+
+# Everything can have an id, a type and a label
+BaseResource._properties = {'id': {"range": str}, 
+	'type': {"rdf": "rdf:type", "range": str}, 
+	'label': {"rdf": "rdfs:label", "range": str}}
+
+fh = codecs.open('crm_vocab.tsv', 'r', 'utf-8')
+lines = fh.readlines()
+fh.close()
+lines = lines[1:] # strip header
+vocabData = {}
+
+for l in lines:
+	info= l.split('\t')
+	name = info[0]	
+	if info[1] == "class":
+		data = {"subOf": info[4], "label": info[2], "desc": info[3], "class": None, "props": [], "subs": []}
+		# calculate CamelCase name
+		uc1 = name.find("_")
+		ccname = name[uc1+1:]
+		ccname = ccname.replace('-', '').replace('_', '')
+		data['className'] = ccname
+		vocabData[name] = data
+	else:
+		# property
+		data = {"name": name, "subOf": info[4], "label": info[2], "desc": info[3], "range": info[6]}
+		uc1 = name.find("_")
+		ccname = name[uc1+1:]
+		what = vocabData[info[5]]
+		data['propName'] = ccname
+		what["props"].append(data)
+
+# invert subclass hierarchy
+for k, v in vocabData.items():
+	sub = v['subOf']
+	try:
+		vocabData[sub]['subs'].append(k)
+	except:
+		pass
+
+# Build class heirarchy from top
+
+def build_class(crmName, parent):
+	data = vocabData[crmName]
+	name = str(data['className'])
+	sub = data['subOf']
+	c = type(name, (parent,), {})
+	globals()[name] = c
+	data['class'] = c
+	c._type = "crm:%s" % crmName
+	c._description = data['desc']
+	c._uri_segment = name
+	c._properties = {}
+	c._classhier = inspect.getmro(c)[:-1]
+
+	# Set up real properties
+	for p in data['props']:
+		name = p['name']
+		rng = p['range']
+		ccname = p['propName']
+		# can't guarantee classes have been built yet :(
+		c._properties[ccname] = {"rdf": "crm:%s" % name, "rangeStr": rng}
+
+	# Build subclasses
+	for s in data['subs']:
+		build_class(s, c)
+
+build_class('E1_CRM_Entity', BaseResource)
+
+for v in vocabData.values():
+	c = v['class']
+	for p in c._properties.values():
+		try:	
+			p['range'] = vocabData[p['rangeStr']]['class']
+		except:
+			p['range'] = str
+		try:
+			del p['rangeStr']
+		except:
+			pass
+
+# Add some extras outside of the ontology
+SymbolicObject._properties['value'] = {"rdf": "rdfs:value", "range": str}
+TimeSpan._properties['begin_of_the_begin'] = {"rdf": "crm:p82a_begin_of_the_begin", "range":str}
+TimeSpan._properties['begin_of_the_end'] = {"rdf": "crm:p81b_begin_of_the_end", "range":str}
+TimeSpan._properties['end_of_the_begin'] = {"rdf": "crm:p81a_end_of_the_begin", "range":str}
+TimeSpan._properties['end_of_the_end'] = {"rdf": "crm:p82b_end_of_the_end", "range":str}
+
+# And now can create subclasses like normal
+
+class TMSIdentifer(Identifier):
+	pass
+
+
