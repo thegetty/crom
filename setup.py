5e6bb6d206ae7e1b917d35843797fa6b90174507
Rob Sanderson <azaroth42@gmail.com>
2017-06-22 15:51:44 -0700

    Remove bad inverses, add P15 as okay

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
         'data/key_order.json', 'data/context.jsonld']
     },
     test_suite="tests",
-    version = '0.4.0',
+    version = '0.4.2',
     description = 'A library for mapping CIDOC-CRM classes to Python objects',
     author = 'Getty Research Institute, Rob Sanderson',
     author_email = 'jgomez@getty.edu, rsanderson@getty.edu',
