51e0e61f4e53690b690685fc4adc07cd78235330
Rob Sanderson <azaroth42@gmail.com>
2017-06-28 16:59:29 -0700

    more tests

--- tests/test_reader.py
+++ tests/test_reader.py
@@ -8,12 +8,15 @@ except:
 	from ordereddict import OrderedDict
 
 from cromulent import reader
-from cromulent.model import factory, Person, DataError
+from cromulent.model import factory, Person, DataError, BaseResource
+from cromulent.extra import EoEActivity
 
 class TestReader(unittest.TestCase):
 
 	def setUp(self):
 		self.reader = reader.Reader()
+		# ensure we can use parent_of
+		Person._properties['parent_of']['okayToUse'] = 1
 
 	def test_read(self):
 		self.assertRaises(DataError, self.reader.read, "")
@@ -22,4 +25,20 @@ class TestReader(unittest.TestCase):
 
 		whostr = '{"type": "Person", "label": "me"}'
 		self.assertTrue(isinstance(self.reader.read(whostr), Person))
-		
+
+		whostr = '{"@context": "fishbat", "type": "Person", "label": "me"}'
+		self.assertTrue(isinstance(self.reader.read(whostr), Person))
+
+		levelstr = '{"type": "Person", "parent_of": {"type": "Person", "label": "child"}}'
+		self.assertTrue(isinstance(self.reader.read(levelstr).parent_of, Person))
+
+		basestr = '{"label": "base"}'
+		self.assertTrue(isinstance(self.reader.read(basestr), BaseResource))
+
+		eoeact = '{"type": ["EndOfExistence", "Activity"], "label":"boo crm"}'
+		self.assertTrue(isinstance(self.reader.read(eoeact), EoEActivity))
+
+		unknown = '{"type":"FishBat"}'
+		self.assertRaises(DataError, self.reader.read, unknown)
+
+
