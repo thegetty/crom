6a3f59fca0a32e2944c64ee4d355b4f5b9f5b058
Rob Sanderson <azaroth42@gmail.com>
2017-06-29 15:32:10 -0700

    Allow line length based string output compaction

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
         'data/key_order.json', 'data/context.jsonld']
     },
     test_suite="tests",
-    version = '0.5.1',
+    version = '0.5.5',
     description = 'A library for mapping CIDOC-CRM classes to Python objects',
     author = 'Getty Research Institute, Rob Sanderson',
     author_email = 'jgomez@getty.edu, rsanderson@getty.edu',
