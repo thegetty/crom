a598c59f17d79098b3f57e8f71fe3071e019fb83
Rob Sanderson <azaroth42@gmail.com>
2019-08-29 09:12:39 -0700

    version bump

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup(
         'data/cidoc-extension.json', 'data/crm-profile.json']
     },
     test_suite="tests",
-    version = '0.14.0',
+    version = '0.14.1',
     description = 'A library for mapping CIDOC-CRM classes to Python objects',
     author = 'Rob Sanderson',
     author_email = 'rsanderson@getty.edu',
