685057d84eaf2492acfb2e4dea7a89c57df7dea5
Gregory Williams <gwilliams@getty.edu>
2019-11-27 15:55:15 -0800

    New Group classes to allow modeling of sales attribution modifiers.

--- cromulent/vocab.py
+++ cromulent/vocab.py
@@ -178,6 +178,10 @@ ext_classes = {
 	"AuctionHouseOrg": {"parent": Group, "id": "300417515", "label": "Auction House (organization)"},
 	"MuseumOrg":   {"parent": Group, "id":"300312281", "label": "Museum"},
 	"Department":  {"parent": Group, "id":"300263534", "label": "Department"},
+	"School":      {"parent": Group, "id":"300404284", "label": "School"},
+	"Circle":      {"parent": Group, "id":"300404283", "label": "Circle"},
+	"Workshop":    {"parent": Group, "id":"300404274", "label": "Workshop"},
+	"Atelier":     {"parent": Group, "id":"300404277", "label": "Atelier"},
 
 	"Nationality": {"parent": Type, "id":"300379842", "label": "Nationality"},
 	"Gender":      {"parent": Type, "id":"300055147", "label": "Gender"},
